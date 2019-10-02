fc0c03acfd0b53fa9ce1f433f8977b5962ea5e69
vanonselenp <vanonselen.p@gmail.com>
2019-10-02 11:56:01 +0200

    working through the udemy course

--- ex01/app.js
+++ ex01/app.js
@@ -1,1 +1,72 @@
-console.log("It works!");
+"use strict";
+// types:
+// strings
+var myName = "Max";
+// myName = 28;
+//numbers
+var myAge = 28;
+// myAge = ""
+// boolean
+var myBool = true;
+// myBool = 1;
+// assign types?
+var myRealAge;
+myRealAge = 27;
+myRealAge = "27";
+var myTypedAge;
+myTypedAge = 27;
+// myTypedAge = "27";
+// arrays
+var hobbies = ["cooking", "sports"];
+console.log(typeof hobbies);
+hobbies = [100];
+//tuples
+var fake_address = ["superstreet", 99];
+// let fake_address_failing: [string, number] = [99, "superstreet"]
+// enums
+var Color;
+(function (Color) {
+    Color[Color["Green"] = 0] = "Green";
+    Color[Color["Gray"] = 1] = "Gray";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+var myColor = Color.Green;
+console.log(myColor);
+// any
+var car = "BMW";
+console.log(car);
+car = { brand: "BMW", series: 3 };
+console.log(car);
+// functions
+function returnMyName() {
+    return myName;
+}
+console.log(returnMyName());
+function test(name) {
+    return name;
+}
+console.log(test("hello"));
+// void
+function sayHello() {
+    console.log("Hello from void");
+}
+sayHello();
+function multiply(value1, value2) {
+    return value1 * value2;
+}
+//functions as types
+var myMultiply;
+// myMultiply = sayHello;
+// myMultiply();
+myMultiply = multiply;
+console.log(myMultiply(2, 5));
+// objects
+var userData = {
+    name: "Peter",
+    age: 30
+};
+var userDataTyped = {
+    name: "Peter",
+    age: 30
+};
+// userData = {};
--- ex01/app.ts
+++ ex01/app.ts
@@ -1,1 +1,82 @@
-console.log("It works!");
+// types:
+// strings
+let myName = "Max";
+// myName = 28;
+
+//numbers
+let myAge = 28;
+// myAge = ""
+
+// boolean
+let myBool = true;
+// myBool = 1;
+
+// assign types?
+let myRealAge;
+myRealAge = 27;
+myRealAge = "27";
+
+let myTypedAge: number;
+myTypedAge = 27;
+// myTypedAge = "27";
+
+// arrays
+let hobbies: any[] = ["cooking", "sports"];
+console.log(typeof hobbies);
+hobbies = [100]
+
+//tuples
+let fake_address: [string, number] = ["superstreet", 99]
+// let fake_address_failing: [string, number] = [99, "superstreet"]
+
+// enums
+enum Color { Green, Gray, Blue }
+let myColor: Color = Color.Green;
+console.log(myColor);
+
+// any
+let car: any = "BMW";
+console.log(car);
+car = {brand: "BMW", series: 3};
+console.log(car);
+
+// functions
+function returnMyName(): string {
+    return myName;
+}
+console.log(returnMyName());
+
+function test(name: string): string {
+    return name;
+}
+console.log(test("hello"));
+
+// void
+function sayHello(): void {
+    console.log("Hello from void");
+}
+sayHello();
+
+function multiply(value1: number, value2: number): number {
+    return value1 * value2;
+}
+
+//functions as types
+let myMultiply: (x: number, y: number) => number;
+// myMultiply = sayHello;
+// myMultiply();
+myMultiply = multiply;
+console.log(myMultiply(2, 5));
+
+// objects
+let userData = {
+    name: "Peter",
+    age: 30
+};
+
+let userDataTyped: { name: string, age: number } = {
+    name: "Peter",
+    age: 30
+};
+// userData = {};
+
